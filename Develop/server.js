62209a95c4b17d1ca0c2515bfacfb1799f10877b
Mostafa <mojtahedimostafa.m@gmail.com>
2020-09-26 13:29:56 +0330

    server.js file modified

--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
@@ -16,14 +17,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
 // routes
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
-});
+
+mongoose.connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`App running on port ${PORT}!`);
+        });
+    })
+    .catch(e => {
+        console.log(e.message);
+    })
+
+
